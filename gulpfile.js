178607178cea21631bd6c03e266266972b9e0cee
Waldemar <waldemarmuhl@gmail.com>
2017-01-19 21:07:06 +0200

    Added gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var bowerFiles = require('main-bower-files');
+var inject = require('gulp-inject');
+
+gulp.task('inject', function () {
+    console.log('Injecting bower files');
+    gulp.src('./app/index.html')
+    .pipe(inject(gulp.src(bowerFiles(), {read: false}), {name: 'bower'}))
+    .pipe(inject(gulp.src('./app/css/*.css', {read: false})))
+    .pipe(gulp.dest('./'));
+})
+
+gulp.task('build', ['inject'], function() {
+    console.log('Building HTML');
+})
+
+gulp.task('serve', ['inject'], function() {
+    browserSync.init({
+        server: './'
+    });
+    gulp.watch('./app/index.html', ['build']);
+    gulp.watch(['./index.html', './app/css/*.css']).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
